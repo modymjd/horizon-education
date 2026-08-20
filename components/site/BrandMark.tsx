e8c2f91c0b50d285e821ed5a5c29ee1d4d271c9f
modymjd <mody.mickey012@gmail.com>
2026-08-20 20:01:52 +0300

    Replace H badge with the Horizon Education logo mark

--- components/site/BrandMark.tsx
+++ components/site/BrandMark.tsx
@@ -3,10 +3,11 @@
 export function BrandMark() {
   return (
     <Link href="/" className="mark" aria-label="Horizon Education">
-      <span className="mark-symbol">H</span>
+      <img src="/logos/horizon-logo.png" alt="Horizon Education" className="mark-logo" />
       <span className="mark-word">
         Horizon <b>Education</b>
       </span>
     </Link>
   )
 }
+
